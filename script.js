2d00713b0e4f98d14e039501203fba0ccb15568b
YOUR FULL NAME <andrewscott@Andrews-MacBook-Air.local>
2020-06-04 14:47:25 -0500

    Add

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 var year = document.getElementById('copyright');
 var today = new Date();
 year.textContent = today.getFullYear();
+
+function myNew () {
+
+var food = prompt("Enter the Food","Turkey");
+    if (person != null) 
+    {
+        document.getElementById("myNew").innerHTML =
+        "Your " + myNew + " is on sale";
+    }
+}
