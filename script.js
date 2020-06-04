89984da5a2fe1c7e1437d9fca458251ad1110c95
YOUR FULL NAME <andrewscott@Andrews-MacBook-Air.local>
2020-06-04 18:26:32 -0500

    Added Function

--- script.js
+++ script.js
@@ -4,13 +4,19 @@ year.textContent = today.getFullYear();
 
 function myNew()
 {
-    var food = prompt("Enter the Food","Turkey");
-        if (food != null) 
-            { document.getElementById("one").innerHTML = 
-                "Your " + food + "  is on sale!";
-            }   
-        else {
-        food = "Please enter in a food";
-             }    
-
+    var food = prompt("Enter the Food");
+    while (food == '') 
+    { 
+        food = prompt ("Please enter in a food");
+    }   
+    document.getElementById("one").innerHTML = "Your " + food + "  is on sale!";
 }
+function pow (x,y){
+    var result = 1;
+    for(var i = 0; i < y; i++){
+        result *= x;
+    }
+    return result;
+}
+console.log(pow(2,4));
+console.log(pow(5,5));
