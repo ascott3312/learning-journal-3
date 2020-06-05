7b3b3d01a08888d8b2d7d69c3f910976abc1a2f9
YOUR FULL NAME <andrewscott@Andrews-MacBook-Air.local>
2020-06-05 01:29:06 -0500

    add

--- script.js
+++ script.js
@@ -5,11 +5,21 @@ year.textContent = today.getFullYear();
 function myNew()
 {
     var food = prompt("Enter the Food");
-    while (food == '') 
+    while (food == '', food > -1) 
     { 
-        food = prompt ("Please enter in a food");
+        food = prompt("Please enter in a food");
     }   
     document.getElementById("one").innerHTML = "Your " + food + "  is on sale!";
+    var price;
+    var quantity;
+    var total;
+        
+    price = 1;
+    quantity = 2.5;
+    total = price * quantity;
+        
+    var el = document.getElementById('cost');
+    el.textContent = '$' + total;
 }
 function pow (x,y){
     var result = 1;
@@ -21,4 +31,12 @@ function pow (x,y){
 console.log(pow(2,4));
 console.log(pow(5,5));
 
-
+function myNew2()
+{
+    var items = prompt("Enter the number items you want us to deliver");
+    while (items == '') 
+    { 
+        items = prompt("Please enter in a number");
+    }   
+    document.getElementById("two").innerHTML = "You ordered " + items + ".";
+}
